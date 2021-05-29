0bc3ea7acb712be6cee0ee9734d85a808ec5bbb8
optimizion <optimizion@gmail.com>
2021-05-29 17:03:33 +0900

    About 페이지 추가

--- src/store/about.js
+++ src/store/about.js
@@ -1,1 +1,10 @@
- 
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'OPTIMY',
+    email: 'optimizion@gmail.com',
+    blog: 'https://optimizion.tistory.com',
+    phone: '+82-10-1234-5678',
+    image: 'https://github.com/optimizion.png'
+  })
+}
