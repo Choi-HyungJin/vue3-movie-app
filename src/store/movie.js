bc387babc166b4b0adcfa9fdbbd827e26446650e
optimizion <optimizion@gmail.com>
2021-05-29 13:57:04 +0900

    비동기 영화 검색기능 추가

---
+++ src/store/movie.js
@@ -0,0 +1,85 @@
+import axios from 'axios'
+import _uniqBy from 'lodash/uniqBy'
+
+export default {
+  namespaced: true,
+  state: () => ({
+    movies: [],
+    message: '',
+    loading: false
+  }),
+  getters: {
+  },
+  mutations: {
+    updateState(state, payload) {
+      // ['movies', 'message', 'loading']
+      Object.keys(payload).forEach(key => {
+        state[key] = payload[key]
+      })
+    },
+    resetMovies(state) {
+      state.movies = []
+    }
+  },
+  actions: {
+    async searchMovies({state, commit}, payload) {
+      try {
+        const res = await _fetchMovies({
+          ...payload,
+          page: 1
+        })
+        const {Search, totalResults} = res.data
+        commit('updateState', {
+          movies: _uniqBy(Search, 'imdbID')
+        })
+        console.log(totalResults); // 272
+        console.log(typeof totalResults); // string
+  
+        const total = parseInt(totalResults, 10)
+        const pageLength = Math.ceil(total / 10)
+  
+        // 추가 요청!
+        if (pageLength > 1) {
+          for (let page = 2; page <= pageLength; page += 1) {
+            if (page > (payload.number / 10)) break
+            const res = await _fetchMovies({
+              ...payload,
+              page
+            })
+            const {Search} = res.data
+            commit('updateState', {
+              movies: [
+                ...state.movies, 
+                ..._uniqBy(Search, 'imdbID')
+              ]
+            })
+          }
+        }
+      } catch (message) {
+        commit('updateState', {
+          movies: [],
+          message
+        })
+      }
+    }
+  }
+}
+
+function _fetchMovies(payload) {
+  const {title, type, year, page} = payload
+  const OMDB_API_KEY = '2d347c70'
+  const url = `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
+  
+  return new Promise((resolve, reject) => {
+    axios.get(url)
+    .then(res => {
+      if (res.data.Error) {
+        reject(res.data.Error)
+      }
+      resolve(res)
+    })
+    .catch(err => {
+      reject(err.message)
+    })
+  })
+}
