52c677b7c4835699c1a4e39bf03c71b8c4937738
optimizion <optimizion@gmail.com>
2021-05-29 17:29:17 +0900

    반응형 추가

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -15,6 +15,13 @@
         </RouterLink>
       </div>
     </div>
+    <div
+      @click="toAbout"
+      class="user">
+      <img
+        :src="image"
+        :alt="name" />
+    </div>
   </header>
 </template>
 
@@ -43,24 +50,64 @@ export default {
       ]
     }
   },
+  computed: {
+    name() {
+      return this.$store.state.about.name
+    },
+    image() {
+      return this.$store.state.about.image
+    }
+  },
   methods: {
     isMatch(path) {
       if (!path) return false
       console.log(this.$route);
       return path.test(this.$route.fullPath)
+    },
+    toAbout() {
+      this.$router.push('/about')
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
+@import "~/scss/main";
 header {
   height: 70px;
   padding: 0 40px;
   display: flex;
   align-items: center;
+  position: relative;
   .logo {
     margin-right: 40px;
+  }
+  .user {
+    width: 40px;
+    height: 40px;
+    padding: 6px;
+    border-radius: 50%;
+    box-sizing: border-box;
+    background-color: $gray-200;
+    cursor: pointer;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 40px;
+    margin: auto;
+    transition: 0.4s;
+    &:hover {
+      background-color: darken($gray-200, 10%);
+    }
+    img {
+      width: 100%;
+      border-radius: 50%;
+    }
+  }
+  @include media-breakpoint-down(sm) {
+    .nav {
+      display: none;
+    }
   }
 }
 </style>
