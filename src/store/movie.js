abb235859115f03fe4313012e2cc0de399fc6006
optimizion <optimizion@gmail.com>
2021-05-29 19:01:21 +0900

    영화 정보 검색 함수 API - netlify functions

--- src/store/movie.js
+++ src/store/movie.js
@@ -67,7 +67,7 @@ export default {
             })
           }
         }
-      } catch (message) {
+      } catch ({ message }) {
         commit('updateState', {
           movies: [],
           message
@@ -105,23 +105,6 @@ export default {
   }
 }
 
-function _fetchMovies(payload) {
-  const {title, type, year, page, id} = payload
-  const OMDB_API_KEY = '2d347c70'
-  const url = id 
-    ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}` 
-    : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
-  
-  return new Promise((resolve, reject) => {
-    axios.get(url)
-    .then(res => {
-      if (res.data.Error) {
-        reject(res.data.Error)
-      }
-      resolve(res)
-    })
-    .catch(err => {
-      reject(err.message)
-    })
-  })
+async function _fetchMovies(payload) {
+  return await axios.post('/.netlify/functions/movie', payload)
 }
